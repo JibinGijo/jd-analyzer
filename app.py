5c219fba153ec5ae1e69e15ff7644aa6d40d83fe
Jibin Gijo <jibingijo123@gmail.com>
2026-05-26 15:29:46 +0530

    added pdf upload and auto skill extraction v2

--- app.py
+++ app.py
@@ -1,21 +1,51 @@
 import streamlit as st
 from transformers import pipeline
+import pdfplumber
 
 st.title("JD Analyzer")
 st.subheader("Resume and Job Description Matching")
 
+
+common_skills = ["Python", "Java", "React", "Machine Learning", "SQL", "MySQL",
+                 "Docker", "FastAPI", "NLP", "Computer Vision", "Flask", "TypeScript",
+                 "Arduino", "ESP32", "Supabase", "PostgreSQL", "C++"]
+
+
 jd=st.text_area("Paste Job Description")
-resume=st.text_area("Paste Resume")
-skills=st.text_area("Enter your skills separated by commas (e.g. Python, ML, React)")
+uploaded_file = st.file_uploader("Upload Resume PDF", type="pdf")
+
 
 if (st.button("Analyze")):
-    if jd and resume and skills:
-        skilllist = list(map(str.strip,skills.split(",")))
+    if jd and uploaded_file:
+        with pdfplumber.open(uploaded_file) as pdf:
+            resume_text=""
+            for page in pdf.pages:
+                resume_text+=page.extract_text()
         classifier=pipeline("zero-shot-classification")
-        results=classifier(jd,skilllist)
-        st.write("results")
+        resume_results=classifier(resume_text,common_skills)
+        extracted_skills=[label for label,score in zip(resume_results["labels"],resume_results["scores"]) if score>0.05]
+
+        st.write("### Skills found in your Resume:")
+        st.write(",".join(extracted_skills))
+
+        results=classifier(jd,extracted_skills)
+
+        matched=[]
+        missing=[]
+
         for label , score in zip(results["labels"],results["scores"]):
-            st.write(f"{label} - {round(score*100,2)}")
+            if score>0.1:
+                matched.append(f"{label} - {round(score*100, 2)}%")
+            else:
+                missing.append(label)
+        
+        st.write("Matched Skills:")
+        for m in matched:
+            st.success(m)
+        
+        st.write("Missing Skills:")
+        for m in missing:
+            st.error(m)
     else:
         st.warning("fill all details")
 
